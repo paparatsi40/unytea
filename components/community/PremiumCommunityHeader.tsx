266bb151a01783c6ad3d55a244145981f884c84e
Carlos <alfaroc@live.com>
2025-12-18 15:04:02 -0800

    Remove unused Info import

--- components/community/PremiumCommunityHeader.tsx
+++ components/community/PremiumCommunityHeader.tsx
@@ -3,7 +3,7 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { CommunityActions } from "@/components/community/CommunityActions";
-import { MessageCircle, Trophy, Users, Info, Heart, TrendingUp, Hash, Settings, Crown } from "lucide-react";
+import { MessageCircle, Trophy, Users, Heart, TrendingUp, Hash, Settings, Crown } from "lucide-react";
 
 type Community = {
   id: string;
