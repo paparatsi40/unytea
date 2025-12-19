5b1f006d626b0f18d95a42bfbaabb5eb4c943f05
Carlos <alfaroc@live.com>
2025-12-18 19:02:04 -0800

    Fix: Remove unused React import from SectionBuilder

--- components/section-builder/SectionBuilder.tsx
+++ components/section-builder/SectionBuilder.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ChevronUp, ChevronDown, Trash2, Copy, Save } from "lucide-react";
 import { SectionInstance, SectionType, FieldDef } from "./types";
 import { SECTIONS, SECTION_ORDER } from "./sections";
