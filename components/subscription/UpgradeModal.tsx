38edccc754c63256e544ddaf7e1595c37682c5b9
Carlos <alfaroc@live.com>
2025-12-18 21:27:05 -0800

    Fix: Remove unused useState import from UpgradeModal

--- components/subscription/UpgradeModal.tsx
+++ components/subscription/UpgradeModal.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import { X, Zap, Check, ArrowRight, TrendingUp } from "lucide-react";
 import Link from "next/link";
 import { SubscriptionPlan } from "@/lib/subscription-plans";
