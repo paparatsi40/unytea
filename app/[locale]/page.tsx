088db6e7854a612492e59c98973ebd7fe612d715
Alfaro <alfaroc@live.com>
2026-03-10 07:39:32 -0700

    Fix: Add LanguageSelector import

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useTranslations } from "next-intl";
+import { LanguageSelector } from "@/components/language-selector";
 import {
   Sparkles,
   LayoutDashboard,
